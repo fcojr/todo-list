d9641e01566399cb68db083c930845135851684f
fcojr <fco-jr@live.com>
2018-02-03 01:31:21 -0200

    Updates

--- js/views/TaskView.js
+++ js/views/TaskView.js
@@ -20,8 +20,8 @@ class TaskView {
                         <td>${DateHelper.dateToText(taskList.dueDate)}</td>
                         <td>${DateHelper.dateToText(taskList.creationTime)}</td>
                         <td>
-                            <button onclick=taskController.markAsDone(${taskList.id}) class="btn btn-sm btn-dark"><i class="fas fa-check"></i></button>
-                            <button onclick="taskController.removeItem(${taskList.id})" class="btn btn-sm btn-dark"><i class="fas fa-times"></i></button>
+                            <button onclick=taskController.markAsDone(${taskList.id}) class="btn btn-sm btn-dark" title="Mark task as done"><i class="fas fa-check"></i></button>
+                            <button onclick="taskController.removeItem(${taskList.id})" class="btn btn-sm btn-dark" title="Remove task"><i class="fas fa-times"></i></button>
                         </td>
                     </tr>`
                 ).join('')}
@@ -44,16 +44,50 @@ class TaskView {
                         <td>${DateHelper.dateToText(doneTasks.dueDate)}</td>
                         <td>${DateHelper.dateToText(doneTasks.creationTime)}</td>
                         <td>
-                            <button onclick="taskController.backTodo(${doneTasks.id})" class="btn btn-sm btn-dark"><i class="fas fa-undo"></i></i></button>
+                            <button onclick="taskController.backTodo(${doneTasks.id})" class="btn btn-sm btn-dark" title="Undo task"><i class="fas fa-undo"></i></i></button>
                         </td>
                     </tr>`
                 ).join('')}
             </table> `
     }
+    noTasks(){
+        return `
+        <table class="table table-striped">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>Task</th>
+                        <th>Due Date</th>
+                        <th>Creation Time</th>
+                        <th>Change Status</th>
+                    </tr>
+                </thead>
+            </table>
+            <p>No tasks</p>
+        `
+    }
+    noDoneTasks(){
+        return `
+        <table class="table table-striped">
+                <thead class="thead-dark">
+                    <tr>
+                        <th class="bg-success">Task</th>
+                        <th class="bg-success">Due Date</th>
+                        <th class="bg-success">Creation Time</th>
+                        <th class="bg-success">Change Status</th>
+                    </tr>
+                </thead>
+            </table>
+            <p>No done tasks</p>
+        `
+    }
     update(taskList, doneTasks){
-        this.app.innerHTML = this.todoTable(taskList)
-       // let textnode = document.createTextNode(this.doneTable(taskList));
-        // let doneTable = this.doneTable(doneTasks)
-        this.app2.innerHTML = this.doneTable(doneTasks)
+        if(typeof taskList !== 'undefined' && taskList.tasks.length > 0)
+            this.app.innerHTML = this.todoTable(taskList)
+        else
+            this.app.innerHTML = this.noTasks()
+        if(typeof doneTasks !== 'undefined' && doneTasks.tasks.length > 0)
+            this.app2.innerHTML = this.doneTable(doneTasks)
+        else
+            this.app2.innerHTML = this.noDoneTasks()
     }
 }
